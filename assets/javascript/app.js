a2fe8cf0720e8fbd75687a525968a34fbd99be7f
omickelsen <omickelsen@gmail.com>
2019-03-10 23:28:10 -0600

    html and basic css with some psuedo coded js

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+
+//game of thrones trivia
+
+
+    // jQuery methods go here...
+  
+// function to call the jquery to on load window 
+//var in the game such as: options to choose from, questions that need asked, answers to those questions var trivia
+
+// object questions related to thrones 
+//create an opject that has options: 
+
+
+//I want the game to load on start with a start button that begins the game of thrones triva. The questions will be displayed on after start button is clicked along with a global timer that will count down for the total length of the game. that timer will be 5 minutes long.  correct/incorrect answers will be logged but hidden until the question is over. once all questions have been answered display total score. if user gets a 50% say something witty etc for percentages up to 100 % if user runs out of time show white walker gif/ some dying gif from thrones and say you lose the game of thrones and end game/ restart
+
+//create var trivia with a bunch of different properties that I can call a bunch.
+
